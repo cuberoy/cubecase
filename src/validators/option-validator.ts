4aecdb0f2af99a30c3412b49191315af87031e3a
Roy <113751898+cuberoy@users.noreply.github.com>
2024-05-19 11:52:18 +0800

    Initial commit

---
+++ src/validators/option-validator.ts
@@ -0,0 +1,43 @@
+// bg-blue-950 border-blue-950
+// bg-zinc-900 border-zinc-900
+// bg-rose-950 border-rose-950
+// bg-zinc-100 border-zinc-100
+// bg-pink-200 border-pink-200
+
+import { PRODUCT_PRICES } from '@/config/products'
+
+export const COLORS = [
+  { label: 'Black', value: 'black', tw: 'zinc-900' },
+  { label: 'Blue', value: 'blue', tw: 'blue-950' },
+  { label: 'Rose', value: 'rose', tw: 'rose-950' },
+  { label: 'White', value: 'white', tw: 'zinc-100' },
+  { label: 'Pink', value: 'pink', tw: 'pink-200' },
+] as const
+
+export const MODELS = {
+  name: 'models',
+  options: [
+    { label: 'iPhone X', value: 'iphonex' },
+    { label: 'iPhone 11', value: 'iphone11' },
+    { label: 'iPhone 12', value: 'iphone12' },
+    { label: 'iPhone 13', value: 'iphone13' },
+    { label: 'iPhone 14', value: 'iphone14' },
+    { label: 'iPhone 15', value: 'iphone15' },
+  ],
+} as const
+
+export const MATERIALS = {
+  name: 'material',
+  options: [
+    { label: 'Silicone', value: 'silicone', description: undefined, price: PRODUCT_PRICES.material.silicone },
+    { label: 'Soft Polycarbonate', value: 'polycarbonate', description: 'Scratch-resistant coating', price: PRODUCT_PRICES.material.polycarbonate },
+  ],
+} as const
+
+export const FINISHES = {
+  name: 'finish',
+  options: [
+    { label: 'Smooth Finish', value: 'smooth', description: undefined, price: PRODUCT_PRICES.finish.smooth },
+    { label: 'Textured Finish', value: 'textured', description: 'Soft grippy texture', price: PRODUCT_PRICES.finish.textured },
+  ],
+} as const
